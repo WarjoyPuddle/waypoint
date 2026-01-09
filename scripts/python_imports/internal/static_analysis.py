6a00b8513b0fa5a5da0f2d6461ef63b17dcc1043
Wojciech Kaluza <wkaluza@protonmail.com>
2026-01-09 10:11:20 +0100

    Refactor static analysis code

--- scripts/python_imports/internal/static_analysis.py
+++ scripts/python_imports/internal/static_analysis.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Wojciech Kałuża
+# Copyright (c) 2025-2026 Wojciech Kałuża
 # SPDX-License-Identifier: MIT
 # For license details, see LICENSE file
 
@@ -39,7 +39,9 @@ def get_files_from_compilation_database(preset, cmake_source_dir) -> typing.List
 
 
 def clang_tidy_process_single_file(data) -> typing.Tuple[bool, str, float, str | None]:
-    file, build_dir, project_root_dir, config_path = data
+    file, preset, cmake_source_dir, project_root_dir, config_path = data
+
+    build_dir = build_dir_from_preset(preset, cmake_source_dir)
 
     with contextlib.chdir(project_root_dir):
         start_time = time.time_ns()
