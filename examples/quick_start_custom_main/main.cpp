9e3e2ce3d85b8ba9524ace9c77ec2debd4f247f9
Wojciech Kaluza <wkaluza@protonmail.com>
2026-01-09 10:11:28 +0100

    Add example with custom entry point

---
+++ examples/quick_start_custom_main/main.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2026 Wojciech Kałuża
+// SPDX-License-Identifier: MIT
+// For license details, see LICENSE file
+
+#include "waypoint/waypoint.hpp"
+
+auto main() -> int
+{
+  auto const t = waypoint::TestRun::create();
+
+  auto const results = waypoint::run_all_tests(t);
+
+  if(results.error_count() > 0)
+  {
+    return 1;
+  }
+
+  if(!results.success())
+  {
+    return 1;
+  }
+
+  return 0;
+}
---
+++ examples/quick_start_custom_main/the_answer.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Wojciech Kałuża
+// SPDX-License-Identifier: MIT
+// For license details, see LICENSE file
+
+#include "the_answer.hpp"
+
+namespace deep_thought
+{
+
+auto the_answer() noexcept -> int
+{
+  return 42;
+}
+
+} // namespace deep_thought
