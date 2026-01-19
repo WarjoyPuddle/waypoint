1c27443be42fb3384ac3beb4b437589d2d7d867d
Wojciech Kaluza <wkaluza@protonmail.com>
2026-01-19 04:17:17 +0100

    Add console output to examples

--- examples/quick_start_build_and_install/the_answer.cpp
+++ examples/quick_start_build_and_install/the_answer.cpp
@@ -1,14 +1,18 @@
-// Copyright (c) 2025 Wojciech Kałuża
+// Copyright (c) 2025-2026 Wojciech Kałuża
 // SPDX-License-Identifier: MIT
 // For license details, see LICENSE file
 
 #include "the_answer.hpp"
+
+#include <iostream>
 
 namespace deep_thought
 {
 
 auto the_answer() noexcept -> int
 {
+  std::cout << "The answer is 42" << std::endl;
+
   return 42;
 }
 
